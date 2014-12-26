119fe3609a5cedaae9d4e127886b7942bf6251b7
Julio Novais Raffaine <julio.loxas@gmail.com>
2014-12-26 14:19:00 -0800

    Iniciar o servidor de Go! Muito trabalho pela frente.

---
+++ Go/server.py
@@ -0,0 +1,98 @@
+﻿import zmq
+
+port = 5555
+
+EMPTY = ' '
+BLACK = 'B'
+WHITE = 'W'
+
+BOARD_SIZE = 9
+
+class GoTable:
+    def __init__(self):
+        self._board = [' ' for i in range(BOARD_SIZE) for j in range(BOARD_SIZE)]
+        self._score = dict([(BLACK,0),(WHITE,0)])
+        self.game_over = False
+        self.winner = EMPTY
+
+    def _set(self, row, col, val):
+        self._board[row*BOARD_SIZE+col] = val
+
+    def _get(self, row, col):
+        return self._board[row*BOARD_SIZE+col]
+
+    def _is_bound(self, row, col):
+        return (row < BOARD_SIZE and col < BOARD_SIZE and row >= 0 and col >= 0)
+
+    def move(self, player, row, col):
+        if (not self._is_bound(row,col) or self._get(row,col) <> EMPTY):
+            return False
+
+        self._set(row, col, player)
+        return True
+        #TODO Suponho que aqui venha a aplicacao de algumas regras do jogo, como peca come outra e tal
+        #TODO Calcular também os pontos de cada jogador
+        
+    def print_board(self):
+        return '\n'.join([''.join(self._board[i*BOARD_SIZE:(i+1)*BOARD_SIZE]) for i in range(BOARD_SIZE)]) 
+
+def strip_message(msg):
+    """
+    A mensagem deve ser do formato "PLAYER LINHA COLUNA"
+    onde PLAYER in {'B','W'} e LINHA/COLUNA in range(BOARD_SIZE)
+    """
+    arr = msg.split(' ')
+    if len(arr) < 3:
+        return (False,"Invalid Message")
+
+    who = arr[0]
+    if who <> WHITE and who <> BLACK:
+        return (False,"Invalid Player")
+
+    try:
+        row = int(arr[1])
+        col = int(arr[2])
+
+    except ValueError:
+        return (False,"Invalid position")
+
+    return (True,(who,row,col))
+
+
+
+#COMEÇA A BRINCADEIRA!
+
+ctx = zmq.Context()
+
+action_server = ctx.socket(zmq.REP)
+action_server.bind("tcp://*:%d"%port)
+
+status_publisher = ctx.socket(zmq.PUB)
+status_publisher.bind("tcp://*:%d"%(port+1))
+
+table = GoTable()
+
+active_player = WHITE
+
+while True:
+    msg = action_server.recv_string();
+    (valid, result) = strip_message(msg)
+    if valid:
+        (who, row, col) = result
+        if table.move(who, row, col):
+            action_server.send_string("ACK")
+            if table.game_over:
+                if table.winner == EMPTY:
+                    status_publisher.send_string("GAMEOVER DRAW")
+                else:
+                    status_publisher.send_string("GAMEOVER %s WINS"%table.winner)
+                break
+
+            active_player = BLACK if (active_player == WHITE) else WHITE
+            #TODO Publicar mais info, tipo o placar?
+            status_publisher.send_string("TURN %s TABLE #%s"%(active_player, table.print_board()))
+            continue
+        else:
+            result = "Invalid Move"
+        
+    action_server.send_string("ERROR %s"%result)
--- README.md
+++ README.md
@@ -1,4 +1,39 @@
-servidor_desafio_ia
+﻿servidor_desafio_ia
 ===================
 
 Servidor para rodar os jogos propostos na lista da EC04 e testar quão bem uma IA desempenha frente as demais IAs oferecidas.
+
+Go: Servidor simples para regular partidas de Go.
+PROTOCOLO: 
+ações: $JOGADOR $LINHA $COLUNA (JOGADOR é B ou W, LINHA E COLUNA sao entre (0,8))
+resp: ACK/ERROR %s
+sub: TURN $JOGADOR TABLE #$MESA (MESA É uma sequencia de 9*9 valores, linhas separadas por \n, onde os valores podem ser ' ', 'W', 'B'), usei o caracter # para marcar o inicio da mesa!
+sub: GAMEOVER $RESULTADO (RESULTADO pode ser $JOGADOR WINS! ou DRAW)
+
+FLUXO
+TODO Aperto de mãos inicial para sincronizar subscribers (algo precisa ser feito para reliability tb)
+Jogadores recebem via Subscriber status da mesa e de quem é a vez
+Jogadores usam REQ para enviar ações, e enquanto não receberem ACK, permanece a vez dele (mensagem de erro será retornada caso contrário) - Lembre-se todo send para o socket REQ precisa ter um recv depois.
+Após a ação novo status da mesa e jogador é retornado.
+Caso o jogo encerre, ao invés do status da mesa será retornado gameover
+
+O servidor está usando unicode (utf-8 signed), lembre-se disso nos clientes! Isso pode mudar também, não é necessário, fiz porque fiz!
+
+Exemplo atual de como montar um cliente em Python 2.7 (zmq 2.x também!)
+	import zmq
+	ctx = zmq.Context()
+	ac = ctx.socket(zmq.REQ)
+	ac.connect("tcp://localhost:5555")
+	sub = ctx.socket(zmq.SUB)
+	sub.connect("tcp://localhost:5556")
+	sub.setsockopt_string(zmq.SUBSCRIBE,unicode("")) #importante criar um filtro, mesmo que vazio!!
+	
+	ac.send_string("W 1 1")
+	msg = ac.recv_string() #opcional parametro zmq.NOBLOCK, mas cuidado se nao tiver msg ele joga exceção!
+	if (msg <> "ACK"):
+		print("joga de novo!") #erro!
+	else:
+		result = sub.recv_string() #ou seja, se deu certo ele publica o status da mesa!
+
+	
+	
